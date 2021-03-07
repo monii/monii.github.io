f98f5a3f41c0597aee64e5b1c532e5a8d8c213b1
moniyel@gmail.com <moniyel@gmail.com>
2021-03-07 23:07:27 +0900

    날씨 아이콘 추가

--- src/header.js
+++ src/header.js
@@ -1,5 +1,6 @@
 const COORDS = document.querySelector('.js-city');
-const WEATHER = document.querySelector('.js-weather');
+const WEATHER = document.querySelector('.js-temperatures');
+const HEADER = document.querySelector('header');
 const API_KEY = '711aab91dc475287f78c916e62cd45f8';
 const KEY_COORDS = 'coords';
 const KEY_CITY = 'city';
@@ -29,6 +30,33 @@ function askCoords() {
   navigator.geolocation.getCurrentPosition(handleSuccessGeo, handleFailGeo);
 }
 
+function getWeatherIcon(weather, weatherId) {
+  const i = document.createElement('i');
+  const parseWeatherId = weatherId.toString();
+  if (parseWeatherId.startsWith('2')) {
+    i.className = 'fas fa-bolt js-weather';
+    HEADER.appendChild(i);
+  } else if (parseWeatherId.startsWith('3')) {
+    i.className = 'fas fa-water js-weather';
+    HEADER.appendChild(i);
+  } else if (parseWeatherId.startsWith('5')) {
+    i.className = 'fas fa-cloud-rain js-weather';
+    HEADER.appendChild(i);
+  } else if (parseWeatherId.startsWith('6')) {
+    i.className = 'fas fa-snowflake js-weather';
+    HEADER.appendChild(i);
+  } else if (parseWeatherId.startsWith('7')) {
+    i.className = 'fas fa-smog js-weather';
+    HEADER.appendChild(i);
+  } else if (weatherId > 800) {
+    i.className = 'fas fa-cloud js-weather';
+    HEADER.appendChild(i);
+  } else {
+    i.className = 'fas fa-sun js-weather';
+    HEADER.appendChild(i);
+  }
+}
+
 function getWeather(lat, lon) {
   fetch(
     `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
@@ -37,9 +65,9 @@ function getWeather(lat, lon) {
       return response.json();
     })
     .then((json) => {
-      console.log(json.name);
-      console.log(json.main.temp);
+      console.log(json);
       saveCityLs(json.name);
+      getWeatherIcon(json.weather[0].main, json.weather[0].id);
       COORDS.innerHTML = json.name;
       WEATHER.innerHTML = `${json.main.temp}℃`;
     });
