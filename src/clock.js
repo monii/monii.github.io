f9fcc44e9ca057e9a33f5a7d4edd332cd1d5bd2e
monii <moniyel@gmail.com>
2021-09-13 17:27:58 +0900

    Refactor 날씨 탭 element 수정 및 Date()객체 전역화

--- src/clock.js
+++ src/clock.js
@@ -1,12 +1,12 @@
 const TIME_DOM = document.querySelector('.js-time');
 const DAY_DOM = document.querySelector('.js-day');
 const DAYS = ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'];
+const NOWDATE = new Date();
 
 function getTime() {
-  const now = new Date();
-  let hour = now.getHours(),
-    min = now.getMinutes(),
-    second = now.getSeconds();
+  let hour = NOWDATE.getHours(),
+    min = NOWDATE.getMinutes(),
+    second = NOWDATE.getSeconds();
   hour = hour < 10 ? `0${hour}` : hour;
   min = min < 10 ? `0${min}` : min;
   second = second < 10 ? `0${second}` : second;
@@ -14,11 +14,10 @@ function getTime() {
 }
 
 function getDay() {
-  const nowDay = new Date();
-  const year = nowDay.getFullYear();
-  const month = nowDay.getMonth();
-  const date = nowDay.getDate();
-  const dayLabel = nowDay.getDay();
+  const year = NOWDATE.getFullYear();
+  const month = NOWDATE.getMonth();
+  const date = NOWDATE.getDate();
+  const dayLabel = NOWDATE.getDay();
   DAY_DOM.innerHTML = `${year}년 ${month+1}월 ${date}일 ${DAYS[dayLabel]}`
 }
 
